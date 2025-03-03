14866f2a65318aeb6480938719a5c16ae94a6e21
Roe Nøttseter <roe@nottseter.no>
2025-03-03 15:27:20 +0100

    fixed login system, now checks for whether user is logged in on all sites

--- app.js
+++ app.js
@@ -4,13 +4,34 @@ import express from 'express';
 import session from 'express-session';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import sqlite3 from 'better-sqlite3'
-const db = sqlite3('./db/minigames.db')
 
 const app = express()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const staticPath = path.join(__dirname, 'public')
+
+// Middleware/session (req.session)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: 'secret_key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // This is just because of HTTP vs HTTPS
+}))
+
+// Make sure the user is logged in
+const pathstocheck = [
+  '/',
+  '/index.html',
+  '/games/*'
+]
+for (var i = 0; i < pathstocheck.length; i++) {
+  app.get(pathstocheck[i], checkloggedin, (req, res) => {
+    return res.sendFile(path.join(staticPath))
+  })
+}
 
 
 // Login funcion
@@ -20,31 +41,49 @@ app.post('/login', async (req, res) => {
   let match = false
 
   // Get user from database
-  const userid = sql.getid(username) //NEED TO DEFINE THIS FUNCTION
+  const userid = sql.getid(username)
   if (!userid) {
     return res.status(401).send('Invalid username or password')
   }
-  const user = sql.getuser(userid) //NEED TO DEFINE THIS FUNCTION
+  const user = sql.getuser(userid)
 
   // Check if password matches
+  console.log("user.password:", user.password)
+  console.log("password:", password)
   if (password == user.password) {
     match = true
-  } else { match = false }
+  } else { match = false; console.log("no match") }
 
   // Save login info in session
-  if (match) { //NEED TO DEFINE REQ.SESSION
-    req.session.loggedin = true //NEED TO DEFINE REQ.SESSION
-    req.session.username = user.name //NEED TO DEFINE REQ.SESSION
-    req.session.userid = user.id //NEED TO DEFINE REQ.SESSION
+  if (match) {
+    console.log("MATCHINGGGG")
+    req.session.loggedin = true
+    req.session.username = user.name
+    req.session.userid = user.id
   }
   return res.redirect('/')
+})
+
+function checkloggedin(req, res, next) {
+  if (req.session.loggedi) {
+    console.log('Logged in')
+    return next()
+  } else {
+    console.log('Not logged in')
+    return res.redirect('/login.html')
+  }
+}
+
+// Logout function
+app.get('/logout', checkloggedin, (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
 })
 
 
 // Send all game names to the user
 app.get('/fetchgames', (req, res) => {
-  const sql = db.prepare('select * from game')
-  let games = sql.all()
+  let games = sql.fetchgames()
   res.send(games)
 })
 
--- sql.js
+++ sql.js
@@ -1,6 +1,35 @@
 import bcrypt from 'bcrypt'
 import express from 'express';
 import session from 'express-session';
+import sqlite3 from 'better-sqlite3'
+const db = sqlite3('./db/minigames.db')
 
 const app = express()
+
+export function getid(username) {
+  let sqltext = 'select id from user where name = ?'
+  let sql = db.prepare(sqltext)
+  let rows = sql.all(username)
+  if (rows.length == 0) {
+    return false
+  }
+  return rows[0].id
+}
+
+export function getuser(userid) {
+  let sqltext = 'select id, name, password from user where id = ?'
+  let sql = db.prepare(sqltext)
+  let rows = sql.all(userid)
+  if (rows.length == 0) {
+    return false
+  }
+  return rows[0]
+}
+
+export function fetchgames() {
+  let sqltext = 'select * from game'
+  let sql = db.prepare(sqltext)
+  let games = sql.all()
+  return games
+}
 
